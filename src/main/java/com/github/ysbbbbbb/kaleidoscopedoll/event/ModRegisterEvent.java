fbf5cd79a85304dfe4c16c81af63ab6bf22b294c
InkNight <InkNight@DedNET.CO>
2025-12-06 11:29:21 +0800

    ``` feat(doll): 添加新玩偶 doll_25076 及相关资源
    新增玩偶 HaruMiao（HaruRikoNaGuZiMiao）的相关模型、贴图、语言本地化及成就配置。
    更新了 advancement 条件引用错误，从 candded_town 更正为 canned_town。
    修复部分 JSON 文件中 UV 坐标错误，并修正了一个成就触发条件的格式问题。
    将自定义玩偶计数增加到 27，并注册新玩偶的特殊提示信息。
    ```

--- src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/event/ModRegisterEvent.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/event/ModRegisterEvent.java
@@ -28,7 +28,7 @@ public class ModRegisterEvent {
 
     //自定义玩偶数量
     private static final int CUSTOM_START_ID = 25050;
-    private static final int CUSTOM_DOLL_COUNT = 26;
+    private static final int CUSTOM_DOLL_COUNT = 27;
 
     private static void registerAllVanillaTooltips() {
         registerVanillaTooltips("doll_328", "wine_fox");
@@ -614,6 +614,7 @@ public class ModRegisterEvent {
         registerSpecialTooltips("doll_25073", "playaer_anhesu86");
         registerSpecialTooltips("doll_25074", "playaer_anhesu7");
         registerSpecialTooltips("doll_25075", "playaer_candded_durian_winter");
+        registerSpecialTooltips("doll_25076", "player_guzimiao");
 
     }
 
